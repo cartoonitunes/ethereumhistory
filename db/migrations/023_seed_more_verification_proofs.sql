c6a0c3d7537442c95e6536ad8fdb9114c7ebef27
cartoonitunes <cartoonitunes@users.noreply.github.com>
2026-03-06 18:50:04 -0500

    fix: use correct avsa gist URLs for Unicorn and Grinder verification proofs

--- db/migrations/023_seed_more_verification_proofs.sql
+++ db/migrations/023_seed_more_verification_proofs.sql
@@ -20,11 +20,11 @@ UPDATE contracts SET
   verification_notes = 'One of the first dApps on Ethereum, deployed 9 days after Frontier launch. Users bid ETH to rank sci-fi movies. Compiled with solc v0.1.4, optimizer enabled. 219 bytes runtime.'
 WHERE address = '0xd94badbec21695b7a36abcb979efad0108319d18';
 
--- Unicorn token (avsa's published source + compiler settings)
+-- Unicorn token / Meat Calculator (avsa's published source + compiler settings)
 UPDATE contracts SET
   compiler_language = 'solidity',
   verification_method = 'author_published_source',
-  verification_proof_url = 'https://gist.github.com/alexvandesande',
+  verification_proof_url = 'https://gist.github.com/alexvandesande/3abc9f741471e08a6356#file-meat-calculator',
   verification_notes = 'Source code and compiler settings published by Alex Van de Sande (avsa), the contract author. Created as an April Fool''s 2016 experiment in token mechanics.'
 WHERE address = '0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43e7';
 
@@ -32,6 +32,6 @@ WHERE address = '0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43e7';
 UPDATE contracts SET
   compiler_language = 'solidity',
   verification_method = 'author_published_source',
-  verification_proof_url = 'https://gist.github.com/alexvandesande',
+  verification_proof_url = 'https://gist.github.com/alexvandesande/3abc9f741471e08a6356#file-meat-grinder-association',
   verification_notes = 'DAO controlling the Unicorn Meat grinder rules. Source and compiler settings published by Alex Van de Sande (avsa). Features quadratic voting where each negative vote is worth 4 support votes.'
 WHERE address = '0xc7e9ddd5358e08417b1c88ed6f1a73149beeaa32';
